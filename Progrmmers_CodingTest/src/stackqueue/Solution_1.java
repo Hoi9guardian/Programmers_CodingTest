cc5e64c7d1a4ebf58c97b9102a3f1b8fa7ed82fb
999999999 <999999999@lenovo-legion>
2021-05-22 03:03:54 +0900

    22222

--- Progrmmers_CodingTest/src/stackqueue/Solution_1.java
+++ Progrmmers_CodingTest/src/stackqueue/Solution_1.java
@@ -14,7 +14,7 @@ public class Solution_1 {
 	
 	public static int [] solution(int[] progresses, int[] speeds) {
 		int[] answer = {};
-		
+		if(progresses.length == 0) return new int [0];
 		
 		
 		
