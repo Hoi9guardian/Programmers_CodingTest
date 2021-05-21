8a8091ee743c658dd0a3f2f5c9c1aa27d9d1b545
999999999 <999999999@lenovo-legion>
2021-05-22 02:51:50 +0900

    .gitinore test _1

---
+++ Progrmmers_CodingTest/src/stackqueue/Solution_1.java
@@ -0,0 +1,26 @@
+package stackqueue;
+
+public class Solution_1 {
+	public static void main(String [] args ) {
+/*		작업의 갯수 : progresses 작업 속도 : speeds
+ * 		
+ * 
+ * 
+ * 
+ * 
+ * */
+		
+	}
+	
+	public static int [] solution(int[] progresses, int[] speeds) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+	
+	
+	
+	
+}
