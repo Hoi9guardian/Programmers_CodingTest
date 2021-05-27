6319384016628c05bd3b6714e7336f43c81b6dc6
999999999 <999999999@lenovo-legion>
2021-05-27 23:08:58 +0900

    git test_8_코드 수정

--- Progrmmers_CodingTest/src/stackqueue/Solution_1.java
+++ Progrmmers_CodingTest/src/stackqueue/Solution_1.java
@@ -1,26 +1,51 @@
 package stackqueue;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Solution_1 {
 	public static void main(String [] args ) {
 /*		 : progresses  : speeds
  * 		
  * 
- * 
+ * 깃허브 테스트 중 ....  6_ gitignore _ 깃허브 반영 여부 확인 
  * 
  * 
  * */
 		
 	}
 	
-	public static int [] solution(int[] progresses, int[] speeds) {
-		int[] answer = {};
-		if(progresses.length == 0) return new int [0];
+	public static List solution(int[] progresses, int[] speeds) {
+		
+		Queue<Integer> q = new ConcurrentLinkedQueue<Integer>();
+		
+		for(int i=0; i<progresses.length; i++) {
+			q.add(
+					
+				(100 -progresses[i]%speeds[i] == 0 
+				?(100 - progresses[i]) 
+				:(100 - progresses[i])/speeds[i]+1)
+				
+				);			
+		}
+		// 
+		
+		List<Integer> result = new ArrayList<Integer>();
+		int standard = q.poll();
+		int cnt = 1;
+		
+		while(!q.isEmpty()) {
+			int num = q.poll();
+			
+			
+		}
 		
 		
-		// 깃허브 테스트 중 ....  6_ gitignore _ 깃허브 반영 여부 확인 
 		
 		
-		return answer;
+		return result;
 	}
 	
 	
